aeefaa91bafe8f15e2ad88f472f68fa0f9544516
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2025-04-02 02:43:01 +0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -5,7 +5,6 @@ const burgerMenu = document.querySelector(".menu__list");
 
 
 
-
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     burgerMenu.classList.toggle("active");
